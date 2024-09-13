635cb8089bd44929c1fc302d6c36101a808c353d
Your Name <youremail@example.com>
2024-09-13 17:01:39 +0300

    create My Bloc Observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:book_app/myBlocObserver.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const BooksApp());
 }
-
 
 class BooksApp extends StatelessWidget {
   const BooksApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Container();
   }
 }
