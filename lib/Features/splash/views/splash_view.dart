7ca7c4b208e1b203498539143bbedf050e5885ba
Your Name <youremail@example.com>
2024-09-14 15:20:57 +0300

    Refactor Splash View

--- lib/Features/splash/views/splash_view.dart
+++ lib/Features/splash/views/splash_view.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:book_app/Core/utils/navigate_to_and_remove.dart';
 import 'package:book_app/Features/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'widgets/splash_view_body.dart';
@@ -16,10 +15,36 @@ class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      NavigationHelper.navigateToAndRemoveUntil(context,
-          widget: const HomeView());
+    _buildNavigateToHome();
+  }
+
+  void _buildNavigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushAndRemoveUntil(
+        _buildRouteToHome(),
+        (route) => false,
+      );
     });
+  }
+
+  Route _buildRouteToHome() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => const HomeView(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOutCubicEmphasized;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
   }
 
   @override
@@ -27,4 +52,3 @@ class _SplashViewState extends State<SplashView> {
     return const SplashViewBody();
   }
 }
-
