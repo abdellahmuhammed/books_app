61bceabbaf2e730e1ec41bb067907bbb8f077d7f
Your Name <youremail@example.com>
2024-09-14 19:40:20 +0300

    refactor Books Assets Helper

--- lib/Core/assets_helper/books_assets_helper.dart
+++ lib/Core/assets_helper/books_assets_helper.dart
@@ -1,5 +1,6 @@
 class BooksAssetsHelper{
   static const String imagesBook = 'assets/images/book.jfif';
   static const String imagesLogo = 'assets/images/Logo.svg';
+  static const String testImage = 'assets/images/testImage.png';
 
 }
