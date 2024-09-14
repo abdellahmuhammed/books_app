68c204170932e920690b7c2e0bef5d23efdc045f
Your Name <youremail@example.com>
2024-09-15 00:15:19 +0300

    create Best Seller List View

---
+++ lib/Features/home/home_views/home_Widgets/BestSellerListView.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'BestSellerListViewItem.dart';
+// Widget for displaying the list of best-selling items
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 30, bottom: 15),
+        child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          // Adds a bouncy scrolling effect
+          itemBuilder: (context, index) => const BestSellerListViewItem(),
+          // Builds each best seller item
+          separatorBuilder: (BuildContext context, int index) => const SizedBox(
+            height: 20, // Adds space between items
+          ),
+          itemCount: 6, // Number of best seller items
+        ),
+      ),
+    );
+  }
+}
