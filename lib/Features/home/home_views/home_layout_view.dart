7c69340435c39b0d6f70a322d24e35f50ef13e4d
Your Name <youremail@example.com>
2024-09-14 19:40:14 +0300

    refactor Books Assets Helper

---
+++ lib/Features/home/home_views/home_layout_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'home_Widgets/HomeViewBody.dart';
+
+class HomeLayoutView extends StatelessWidget {
+  const HomeLayoutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
+
+
