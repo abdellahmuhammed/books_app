09b8b69611bb50b6d330d25cb5ae040a343b7a43
Your Name <youremail@example.com>
2024-09-14 16:52:01 +0300

    refactor books Assets

--- lib/Core/generated_assets/books_assets.dart
+++ lib/Core/generated_assets/books_assets.dart
@@ -1,5 +1,5 @@
 class BookAssets{
   static const String imagesBook = 'assets/images/book.jfif';
-  static const String imagesLogo = 'assets/images/Logo.png';
+  static const String imagesLogo = 'assets/images/Logo.svg';
 
 }
