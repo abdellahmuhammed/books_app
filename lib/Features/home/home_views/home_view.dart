2fe6c1d2be010ce3f13ee9d65e9afdfb195f0716
Your Name <youremail@example.com>
2024-09-14 17:18:12 +0300

    create home view

--- lib/Features/home/home_views/home_view.dart
+++ lib/Features/home/home_views/home_view.dart
@@ -1,13 +1,36 @@
+import 'package:book_app/Core/assets_helper/books_assets_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'home_Widgets/CustomHomeViewAppBar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body:   Text('data',style: Theme.of(context).textTheme.titleLarge,),
+    return const Scaffold(
+      body: HomeViewBody(),
     );
   }
 }
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomHomeViewAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
