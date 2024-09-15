cb88221bcf9deb742a6daa0a102d0d3ab946c22f
Your Name <youremail@example.com>
2024-09-15 12:06:20 +0300

    refactor BestSellerListViewItem

--- lib/Features/home/home_views/home_Widgets/BestSellerListViewItem.dart
+++ lib/Features/home/home_views/home_Widgets/BestSellerListViewItem.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
-
 import '../../../../Core/theme/books_theme.dart';
+import 'BestSellerPriceAndRatting.dart';
+import 'CustomImage.dart';
 // Widget representing a single best seller item
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -15,25 +16,11 @@ class BestSellerListViewItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start, // Aligns items at the top
         children: [
           // Image container for the book cover
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                // Rounded corners for the book image
-                image: const DecorationImage(
-                  image: AssetImage('assets/images/book.jfif'),
-                  // Book cover image
-                  fit: BoxFit.fill, // Fill the container with the image
-                ),
-              ),
-            ),
-          ),
+         const CustomImage(assetsImage: 'assets/images/book.jfif',),
           // Adds space between the image and the text
           const SizedBox(
             width: 30,
           ),
-
           // Column for the book's details (title, author, price, rating)
           Expanded(
             child: Column(
@@ -44,7 +31,7 @@ class BestSellerListViewItem extends StatelessWidget {
                   child: Text(
                     'Harry Potter and the Goblet of Fire',
                     maxLines: 2,
-                    overflow:TextOverflow.ellipsis,
+                    overflow: TextOverflow.ellipsis,
                     style: BooksStyles.textStyle20Medium(context),
                   ),
                 ),
@@ -58,7 +45,7 @@ class BestSellerListViewItem extends StatelessWidget {
                       .copyWith(color: Colors.grey),
                 ),
                 // Row to display price and rating
-                _buildPriceAndRating(context),
+                const BestSellerPriceAndRatting(),
                 // Adds a small gap
               ],
             ),
@@ -67,27 +54,6 @@ class BestSellerListViewItem extends StatelessWidget {
       ),
     );
   }
-
-  Row _buildPriceAndRating(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          '19.99 €',
-          style: BooksStyles.textStyle20Medium(context),
-        ),
-
-        const Spacer(), // Fills space between price and rating
-        Text(
-          '⭐  2.5 ', // Star rating
-          style: BooksStyles.textStyle16Medium(context),
-        ),
-        Text(
-          '  (500)', // Number of reviews
-          style: BooksStyles.textStyle14Medium(context).copyWith(
-            color: Colors.grey,
-          ),
-        ),
-      ],
-    );
-  }
 }
+
+
