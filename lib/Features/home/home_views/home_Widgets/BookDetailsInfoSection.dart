ed5c88d9eebce06e15798c2929302766d40a8002
Your Name <youremail@example.com>
2024-09-15 17:48:45 +0300

    build Book Details InfoSection

---
+++ lib/Features/home/home_views/home_Widgets/BookDetailsInfoSection.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../../../Core/assets_helper/books_assets_helper.dart';
+import '../../../../Core/theme/books_theme.dart';
+import 'BookRating.dart';
+import 'CustomBookDetailsAppBar.dart';
+import 'CustomImage.dart';
+
+class BookDetailsInfoSection extends StatelessWidget {
+  const BookDetailsInfoSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        const CustomBookDetailsAppBar(),
+        const SizedBox(
+          height: 25,
+        ),
+        CustomImage(
+          assetsImage: BooksAssetsHelper.imagesBook,
+          height: MediaQuery.of(context).size.height * .30,
+          aspectRatio: 3 / 4,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70),
+            child: Text(
+              'The Jungle Book',
+              style: BooksStyles.textStyle30Bold(context),
+              maxLines: 2,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          'The Jungle Book',
+          style: BooksStyles.textStyle18Medium(context)
+              .copyWith(color: Colors.grey),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ],
+    );
+  }
+}
