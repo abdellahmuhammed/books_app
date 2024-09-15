2d018c795f3d4775e827d58e6575c21eed37203b
Your Name <youremail@example.com>
2024-09-15 16:43:38 +0300

    build SimilarBooksItem

---
+++ lib/Features/home/home_views/home_Widgets/SimilarBooksListView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'SimilarBooksItem.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SimilarBooksItem();
+  }
+}
