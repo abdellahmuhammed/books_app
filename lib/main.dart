e33ab2b2f7167807fca4edc9353dd4adf94967b6
Your Name <youremail@example.com>
2024-09-15 11:58:01 +0300

    Refactor main as add Go Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:book_app/Core/theme/book_colors.dart';
+import 'package:book_app/Core/utils/app_router.dart';
 import 'package:book_app/myBlocObserver.dart';
 import 'package:flutter/material.dart';
 import 'Features/splash/splash_views/splash_view.dart';
@@ -14,13 +15,14 @@ class BooksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: BooksColor.kPrimaryColor,
         brightness: Brightness.dark
       ),
-      home:const SplashView(),
+     routerConfig: BookRouter.router,
+     // home:const SplashView(),
     );
   }
 }
