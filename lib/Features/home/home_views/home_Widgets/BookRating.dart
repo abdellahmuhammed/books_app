9b389f4c439ffb15f3298de06fe59f0b53cd10c9
Your Name <youremail@example.com>
2024-09-15 15:29:41 +0300

    create book Ratting

---
+++ lib/Features/home/home_views/home_Widgets/BookRating.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../../Core/theme/books_theme.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key,  this.mainAxisAlignment = MainAxisAlignment. start});
+final MainAxisAlignment mainAxisAlignment;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: [
+        Text(
+          '⭐ 2.5 ', // Star rating
+          style: BooksStyles.textStyle16Medium(context),
+        ),
+        Text(
+          ' (500)', // Number of reviews
+          style: BooksStyles.textStyle14Medium(context).copyWith(
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
