5b3a4333712a42d999a405eab5b0fd379c0622cb
Your Name <youremail@example.com>
2024-09-15 12:14:32 +0300

    refactor BestSeller List View Item

--- lib/Features/home/home_views/home_Widgets/BestSellerListViewItem.dart
+++ lib/Features/home/home_views/home_Widgets/BestSellerListViewItem.dart
@@ -1,6 +1,7 @@
-
+import 'package:book_app/Core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import '../../../../Core/theme/books_theme.dart';
+import '../../../../Core/utils/navigation_helper.dart';
 import 'BestSellerPriceAndRatting.dart';
 import 'CustomImage.dart';
 // Widget representing a single best seller item
@@ -10,50 +11,56 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 105, // Dynamic height based on screen size,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start, // Aligns items at the top
-        children: [
-          // Image container for the book cover
-         const CustomImage(assetsImage: 'assets/images/book.jfif',),
-          // Adds space between the image and the text
-          const SizedBox(
-            width: 30,
-          ),
-          // Column for the book's details (title, author, price, rating)
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // Aligns text to the left
-              children: [
-                SizedBox(
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: BooksStyles.textStyle20Medium(context),
-                  ),
-                ),
-                // Book title
-                const SizedBox(
-                  width: 2,
-                ), // Adds a small gap
-                Text(
-                  'J.K. Rowling',
-                  style: BooksStyles.textStyle14Medium(context)
-                      .copyWith(color: Colors.grey),
-                ),
-                // Row to display price and rating
-                const BestSellerPriceAndRatting(),
-                // Adds a small gap
-              ],
+    return InkWell(
+      onTap: () {
+        BookNavigation.navigateTo(context, location: BookRouter.kBookDetails);
+      },
+      child: SizedBox(
+        height: 105, // Dynamic height based on screen size,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // Aligns items at the top
+          children: [
+            // Image container for the book cover
+            const CustomImage(
+              assetsImage: 'assets/images/book.jfif',
             ),
-          ),
-        ],
+            // Adds space between the image and the text
+            const SizedBox(
+              width: 30,
+            ),
+            // Column for the book's details (title, author, price, rating)
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // Aligns text to the left
+                children: [
+                  SizedBox(
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: BooksStyles.textStyle20Medium(context),
+                    ),
+                  ),
+                  // Book title
+                  const SizedBox(
+                    width: 2,
+                  ), // Adds a small gap
+                  Text(
+                    'J.K. Rowling',
+                    style: BooksStyles.textStyle14Medium(context)
+                        .copyWith(color: Colors.grey),
+                  ),
+                  // Row to display price and rating
+                  const BestSellerPriceAndRatting(),
+                  // Adds a small gap
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
