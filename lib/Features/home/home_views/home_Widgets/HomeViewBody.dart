22ceb7326f81aa45779e1a98169a8ebf6f01ded6
Your Name <youremail@example.com>
2024-09-15 12:06:37 +0300

    refactor HomeViewBody

--- lib/Features/home/home_views/home_Widgets/HomeViewBody.dart
+++ lib/Features/home/home_views/home_Widgets/HomeViewBody.dart
@@ -9,37 +9,41 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      // Adds padding on the horizontal axis (left and right)
-      padding: const EdgeInsets.only(left: 30, right: 30, top: 48),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        // Aligns children to the left
-        children: [
-          // Adds vertical spacing equal to 6% of the screen height
-          const CustomHomeViewAppBar(),
-          // Displays the custom app bar for the home view
-          const SizedBox(
-            height: 46.9,
-          ),
-          // Adds vertical spacing equal to 2% of the screen height
-          const HomeViewFeature(),
-          // Displays the feature section of the home view
-          const SizedBox(
-            height: 49,
-          ),
-          // Adds vertical spacing equal to 4% of the screen height
-          Text(
-            'Best Seller ',
-            style: BooksStyles.textStyle18Bold(context),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // Displays the "Best Seller" title using the current theme's headline style
-          const BestSellerListView(),
-          // Expands the "Best Seller" feature to fill the remaining vertical space
-        ],
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+
+      child: Padding(
+        // Adds padding on the horizontal axis (left and right)
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // Aligns children to the left
+          children: [
+            // Adds vertical spacing equal to 6% of the screen height
+            const CustomHomeViewAppBar(),
+            // Displays the custom app bar for the home view
+            const SizedBox(
+              height: 46.9,
+            ),
+            // Adds vertical spacing equal to 2% of the screen height
+            const HomeViewFeature(),
+            // Displays the feature section of the home view
+            const SizedBox(
+              height: 49,
+            ),
+            // Adds vertical spacing equal to 4% of the screen height
+            Text(
+              'Best Seller ',
+              style: BooksStyles.textStyle18Bold(context),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // Displays the "Best Seller" title using the current theme's headline style
+            const BestSellerListView(),
+            // Expands the "Best Seller" feature to fill the remaining vertical space
+          ],
+        ),
       ),
     );
   }
