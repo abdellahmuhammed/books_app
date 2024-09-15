7c7bf23d53c5ab0bf32cde3265286db11c1d4731
Your Name <youremail@example.com>
2024-09-15 15:44:41 +0300

    build  Search List View

---
+++ lib/Features/search/search_view/search_widget/SearchListView.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../Core/utils/helper.dart';
+import '../../../home/home_views/home_Widgets/BookListViewItem.dart';
+
+class SearchListView extends StatelessWidget {
+  const SearchListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 30, bottom: 15, top: 0),
+        child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          // Adds a bouncy scrolling effect
+          itemBuilder: (context, index) => const BookListViewItem(),
+          // Builds each best seller item
+          separatorBuilder: (BuildContext context, int index) => myDivider(),
+          itemCount: 30, // Number of best seller items
+        ),
+      ),
+    );
+  }
+
+
+}
