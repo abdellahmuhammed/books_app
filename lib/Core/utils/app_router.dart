b0e8957aba499454338c9fce07fbe4c6c069b4d0
Your Name <youremail@example.com>
2024-09-15 12:16:19 +0300

    create Book Details route

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:book_app/Features/home/home_views/home_layout_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../Features/home/home_views/book_details_view.dart';
 import '../../Features/splash/splash_views/splash_view.dart';
 
 abstract class BookRouter {
   static const String kHomeView = '/homeView';
+  static const String kBookDetails = '/bookDetail';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,6 +16,10 @@ abstract class BookRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeLayoutView(),
+      ),
+      GoRoute(
+        path: kBookDetails,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
