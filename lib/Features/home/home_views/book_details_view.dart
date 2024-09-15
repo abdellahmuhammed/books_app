3e644c2876fb59f3165d90c606ac283bc2d059ce
Your Name <youremail@example.com>
2024-09-15 12:15:26 +0300

    build Book Details View

---
+++ lib/Features/home/home_views/book_details_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
