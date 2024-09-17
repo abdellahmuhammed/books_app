38f870675b5cf158513c222cf0410ffd5598bcaf
Your Name <youremail@example.com>
2024-09-17 11:32:58 +0300

    Refactor Splash View

--- lib/Features/splash/splash_views/splash_view.dart
+++ lib/Features/splash/splash_views/splash_view.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: use_build_context_synchronously
 
-
 import 'package:flutter/material.dart';
 import '../../../Core/utils/Book_navigation.dart';
 import '../../../Core/utils/app_router.dart';
-import '../../../Core/utils/route_to_home_with_animation.dart';
+import '../../home/home_views/home_layout_view.dart';
 import 'splash_widgets/splash_view_body.dart';
 
 // The SplashView widget is responsible for displaying the splash screen
@@ -39,4 +38,28 @@ class _SplashViewState extends State<SplashView> {
     });
   }
 
+  Route buildRouteToHome() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          const HomeLayoutView(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin =
+            Offset(0.0, 1.0); // Animation starts from left (outside screen)
+        const end = Offset.zero; // Animation ends at the center of the screen
+        const curve =
+            Curves.easeInOutCubicEmphasized; // A smooth animation curve
+
+        // Creates a Tween to animate the transition from `begin` to `end` using the defined curve
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        // Returns the SlideTransition widget, which moves the screen from right to center
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
+  }
 }
