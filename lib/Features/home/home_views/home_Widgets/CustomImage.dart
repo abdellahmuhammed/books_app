da7077f5c9172988f8b08516a2750344d27c3aa6
Your Name <youremail@example.com>
2024-09-17 23:11:32 +0300

    refactor Custom Image

--- lib/Features/home/home_views/home_Widgets/CustomImage.dart
+++ lib/Features/home/home_views/home_Widgets/CustomImage.dart
@@ -1,29 +1,30 @@
 // ignore_for_file: file_names
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../Core/widgets/custom_circular_indicator.dart';
 
 class CustomImage extends StatelessWidget {
   const CustomImage({
-    super.key, required this.imageUrl, this.aspectRatio = 2.5 / 4, this.height,
+    super.key,
+    required this.imageUrl,
+    this.aspectRatio = 2.5 / 4,
+    this.height,
   });
-  final String imageUrl ;
-  final double aspectRatio ;
-  final double? height ;
+  final String imageUrl;
+  final double aspectRatio;
+  final double? height;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      child: AspectRatio(
-        aspectRatio: aspectRatio,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            // Rounded corners for the book image
-            image:  DecorationImage(
-              image: NetworkImage(imageUrl),
-              // Book cover image
-              fit: BoxFit.fill, // Fill the container with the image
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: SizedBox(
+        height: height,
+        child: AspectRatio(
+          aspectRatio: aspectRatio,
+          child: CachedNetworkImage(imageUrl: 'imageUrl' , fit: BoxFit.fill,
+          errorWidget: (context, url, error)=> const CustomCircularIndicator() ,
           ),
         ),
       ),
