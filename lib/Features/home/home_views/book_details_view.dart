5994f242ca370df356d77748253b6bbb704734fc
Your Name <youremail@example.com>
2024-09-20 18:14:55 +0300

    refactor BookDetailsView with comments

--- lib/Features/home/home_views/book_details_view.dart
+++ lib/Features/home/home_views/book_details_view.dart
@@ -4,9 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../home_view_model/similar_books_cubit/similar_books_cubit.dart';
 import 'home_Widgets/BookDetailsViewBody.dart';
 
+/// Stateful widget that displays detailed information about a book
+/// and fetches similar books using the [SimilarBooksCubit].
 class BookDetailsView extends StatefulWidget {
+  /// Constructor for the [BookDetailsView] widget.
   const BookDetailsView({super.key, required this.bookModel});
-final BookModel bookModel;
+
+  /// The model representing the book whose details will be displayed.
+  final BookModel bookModel;
 
   @override
   State<BookDetailsView> createState() => _BookDetailsViewState();
@@ -15,15 +20,17 @@ final BookModel bookModel;
 class _BookDetailsViewState extends State<BookDetailsView> {
   @override
   void initState() {
-    BlocProvider.of<SimilarBooksCubit>(context).fetchSimilarBooks(
-        categories: widget.bookModel.volumeInfo.categories![0]);
     super.initState();
+    // Fetch similar books using the first category of the book model.
+    BlocProvider.of<SimilarBooksCubit>(context).fetchSimilarBooks(
+      categories: widget.bookModel.volumeInfo.categories![0],
+    );
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: BookDetailsViewBody(bookModel: widget.bookModel,),
+    return Scaffold(
+      body: BookDetailsViewBody(bookModel: widget.bookModel),
     );
   }
 }
-
