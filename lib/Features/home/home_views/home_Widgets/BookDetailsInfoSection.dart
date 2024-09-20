7cd5988063fa76f670e8593b4b082fa26360cf09
Your Name <youremail@example.com>
2024-09-20 18:16:28 +0300

    refactor BookDetailsInfoSection with comments

--- lib/Features/home/home_views/home_Widgets/BookDetailsInfoSection.dart
+++ lib/Features/home/home_views/home_Widgets/BookDetailsInfoSection.dart
@@ -7,35 +7,40 @@ import '../../home_models/home_data_model/book_model.dart';
 import 'CustomBookDetailsAppBar.dart';
 import 'CustomImage.dart';
 
+/// A widget that displays detailed information about a book, including title, author, and cover image.
 class BookDetailsInfoSection extends StatelessWidget {
+  /// Constructor for the [BookDetailsInfoSection] widget.
   const BookDetailsInfoSection({super.key, required this.bookModel});
 
+  /// The model representing the book details to be displayed.
   final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Custom app bar for book details view.
         const CustomBookDetailsAppBar(),
         const SizedBox(
           height: 20,
         ),
+        // Displays the book's cover image.
         CustomImage(
           imageUrl:
-              '${bookModel.volumeInfo.imageLinks?.thumbnail ?? const CustomCircularIndicator()}',
+          '${bookModel.volumeInfo.imageLinks?.thumbnail ?? const CustomCircularIndicator()}',
           height: MediaQuery.of(context).size.height * .30,
           aspectRatio: 3 / 4,
         ),
         const SizedBox(
           height: 30,
         ),
+        // Book title section, centered and styled.
         SizedBox(
           width: double.infinity,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 70),
             child: Text(
               bookModel.volumeInfo.title!,
-
               style: BooksStyles.textStyle30Bold(context),
               maxLines: 2,
               textAlign: TextAlign.center,
@@ -45,6 +50,7 @@ class BookDetailsInfoSection extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
+        // Book author section, if available, otherwise defaults to 'unknown author'.
         Text(
           bookModel.volumeInfo.authors?[0] ?? 'unknown author',
           style: BooksStyles.textStyle18Medium(context)
