03222b2c72c59cafa0f4330901bf9c81751d4590
Your Name <youremail@example.com>
2024-09-20 18:15:57 +0300

    refactor BookActionButtons with comments

--- lib/Features/home/home_views/home_Widgets/BookActionButtons.dart
+++ lib/Features/home/home_views/home_Widgets/BookActionButtons.dart
@@ -5,9 +5,13 @@ import 'package:bookly_app/Features/home/home_models/home_data_model/book_model.
 import 'package:flutter/material.dart';
 import '../../../../Core/widgets/CustomMaterialButton.dart';
 
+/// Widget representing action buttons for a book.
+/// Provides a preview link and other actions related to the book.
 class BookActionButtons extends StatelessWidget {
+  /// Constructor for the [BookActionButtons] widget.
   const BookActionButtons({super.key, required this.bookModel});
 
+  /// The model representing the book for which the action buttons are displayed.
   final BookModel bookModel;
 
   @override
@@ -15,13 +19,15 @@ class BookActionButtons extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        // Button showing the book's availability status as 'Free'.
         const CustomMaterialButton(
-          text: 'Free' ,
+          text: 'Free',
           textColor: Colors.black,
           backgroundColor: Colors.white,
           topLeft: 8,
           bottomLeft: 8,
         ),
+        // Button to launch the book preview link if available.
         CustomMaterialButton(
           onTap: () {
             customLaunchCustomUr(context, bookModel.volumeInfo.previewLink);
@@ -36,9 +42,11 @@ class BookActionButtons extends StatelessWidget {
     );
   }
 
+  /// Returns the appropriate button text based on the availability of the preview link.
+  /// If the link is available, returns 'Free preview'. Otherwise, returns 'Not available now'.
   String customTextAction() {
     return bookModel.volumeInfo.previewLink != null
-        ? 'Free preview '
-        : ' not available now ';
+        ? 'Free preview'
+        : 'Not available now';
   }
 }
