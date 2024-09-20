0b6de51c44138f65a38d1b78c87a50632cf85ede
Your Name <youremail@example.com>
2024-09-20 18:17:06 +0300

    refactor BookDetailsViewBody with comments

--- lib/Features/home/home_views/home_Widgets/BookDetailsViewBody.dart
+++ lib/Features/home/home_views/home_Widgets/BookDetailsViewBody.dart
@@ -7,44 +7,49 @@ import 'BookActionButtons.dart';
 import 'BookDetailsInfoSection.dart';
 import 'SimilarBooksListView.dart';
 
+/// A widget that represents the main body of the book details view,
+/// displaying the book's information, action buttons, and a list of similar books.
 class BookDetailsViewBody extends StatelessWidget {
+  /// Constructor for the [BookDetailsViewBody] widget.
   const BookDetailsViewBody({super.key, required this.bookModel});
-final BookModel bookModel;
+
+  /// The model representing the book details to be displayed.
+  final BookModel bookModel;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
         padding:
-            const EdgeInsets.only(top: 25, left: 30, right: 30, bottom: 10),
+        const EdgeInsets.only(top: 25, left: 30, right: 30, bottom: 10),
         child: Column(
           children: [
-               BookDetailsInfoSection(bookModel: bookModel,),
+            // Displays the book's details information.
+            BookDetailsInfoSection(bookModel: bookModel),
             const SizedBox(
               height: 15,
             ),
-            BookActionButtons(bookModel: bookModel,),
+            // Displays action buttons related to the book (e.g., preview, buy).
+            BookActionButtons(bookModel: bookModel),
             const SizedBox(
               height: 30,
             ),
+            // Header for the similar books section.
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
                 'You can also like',
-                style: BooksStyles.textStyle14Bold(context)
-                    ,
+                style: BooksStyles.textStyle14Bold(context),
               ),
             ),
             const SizedBox(
               height: 16,
             ),
-            SimilarBooksListView(bookModel: bookModel,)
+            // Displays a list of books similar to the current book.
+            SimilarBooksListView(bookModel: bookModel),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
