b99ec0f5f11a7367ed930d2fe370094af69a9bb4
Your Name <youremail@example.com>
2024-09-20 18:11:30 +0300

    refactor HomeRepo with comments

--- lib/Features/home/home_models/home_repo/HomeRepo.dart
+++ lib/Features/home/home_models/home_repo/HomeRepo.dart
@@ -1,14 +1,21 @@
 // ignore_for_file: file_names
 
 import 'package:dartz/dartz.dart';
-
 import '../../../../Core/errors/failure.dart';
 import '../home_data_model/book_model.dart';
 
-abstract class HomeRepo{
-
+/// Abstract repository interface for fetching book data.
+abstract class HomeRepo {
+ /// Fetches featured books.
+ /// Returns either a [Failures] object or a list of [BookModel]s.
  Future<Either<Failures, List<BookModel>>> fetchFeatureBooks();
- Future<Either<Failures, List<BookModel>>>  fetchNewsBooks();
- Future<Either<Failures, List<BookModel>>>  fetchSimilarBooks({required String categories});
 
+ /// Fetches newly released books.
+ /// Returns either a [Failures] object or a list of [BookModel]s.
+ Future<Either<Failures, List<BookModel>>> fetchNewsBooks();
+
+ /// Fetches similar books based on a given category.
+ /// Takes [categories] as a required parameter to filter similar books.
+ /// Returns either a [Failures] object or a list of [BookModel]s.
+ Future<Either<Failures, List<BookModel>>> fetchSimilarBooks({required String categories});
 }
