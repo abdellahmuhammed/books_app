ec6697f7c616f7f450be9dd2e16e123b8d6094a7
Your Name <youremail@example.com>
2024-09-20 18:09:57 +0300

    refactor with comments in CustomErrorMessage

--- lib/Core/widgets/custom_error_message.dart
+++ lib/Core/widgets/custom_error_message.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import '../theme/books_theme.dart';
 
+/// CustomErrorMessage is a widget that displays an error message.
 class CustomErrorMessage extends StatelessWidget {
   const CustomErrorMessage({super.key, required this.errorMessage});
 
-  final String errorMessage;
+  final String errorMessage; // Error message to be displayed
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        errorMessage,
-        textAlign: TextAlign.center,
-        style: BooksStyles.textStyle20Bold(context),
+        errorMessage, // Displayed error message
+        textAlign: TextAlign.center, // Center the text
+        style: BooksStyles.textStyle20Bold(context), // Style for the error message
       ),
     );
   }
