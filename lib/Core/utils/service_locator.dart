b7081c4e83fcfb0021f488b7de73a7a37dd72e02
Your Name <youremail@example.com>
2024-09-20 18:08:22 +0300

    setupServiceLocator

--- lib/Core/utils/service_locator.dart
+++ lib/Core/utils/service_locator.dart
@@ -3,11 +3,15 @@ import 'package:bookly_app/Features/home/home_models/home_repo/HomeRepoImplement
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+// Create a singleton instance of GetIt for service location.
 var gitIt = GetIt.instance;
 
-void setupServiceLocator(){
+/// Sets up the service locator for dependency injection.
+/// Registers the necessary services and repositories.
+void setupServiceLocator() {
+  // Register the ApiServices as a singleton using Dio for HTTP requests.
   gitIt.registerSingleton<ApiServices>(ApiServices(Dio()));
 
+  // Register the HomeRepoImplement as a singleton, providing it with the ApiServices instance.
   gitIt.registerSingleton<HomeRepoImplement>(HomeRepoImplement(gitIt.get<ApiServices>()));
-
 }
